63581afb0b70458ef8e83923a81474bdde466cb4
psychoflute <hitegabriel298@gmail.com>
2026-08-16 20:49:57 -0500

    Add files via upload

---
+++ vroom.h
@@ -0,0 +1,49 @@
+#ifndef DOOM_GENERIC
+#define DOOM_GENERIC
+
+#include <stdlib.h>
+#include <stdint.h>
+
+#ifndef Vroom_RESX
+#define Vroom_RESX 640
+#endif  // Vroom_RESX
+
+#ifndef Vroom_RESY
+#define Vroom_RESY 400
+#endif  // Vroom_RESY
+
+
+#ifdef CMAP256
+
+typedef uint8_t pixel_t;
+
+#else  // CMAP256
+
+typedef uint32_t pixel_t;
+
+#endif  // CMAP256
+
+
+extern pixel_t* DG_ScreenBuffer;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void vroom_Create(int argc, char **argv);
+void vroom_Tick();
+
+
+//Implement below functions for your platform
+void DG_Init();
+void DG_DrawFrame();
+void DG_SleepMs(uint32_t ms);
+uint32_t DG_GetTicksMs();
+int DG_GetKey(int* pressed, unsigned char* key);
+void DG_SetWindowTitle(const char * title);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //DOOM_GENERIC
